b3aac2ef7e74c895c4c6f92eb5b9e9e5d6038314
Thomas <thomas.rwubben@outlook.com>
2026-05-20 17:16:53 +0200

    chore: sync version to 4.1.7

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.1.6";
+        version = "4.1.7";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
