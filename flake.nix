7e4c2579de7858b61469166bfa977cc4afaec0ac
Thomas <thomas.rwubben@outlook.com>
2026-05-21 13:49:55 +0200

    fix: use gh release list instead of gh api for deleting old stable releases

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.6.6";
+        version = "4.6.7";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
