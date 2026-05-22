696dbb1fa9e9b4e2e61137a037e647bca352b61f
Thomas <thomas.rwubben@outlook.com>
2026-05-22 18:59:11 +0200

    fix: patch tao for macOS 26 on c5a6b4d (last commit before liquid glass)

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.21.1";
+        version = "4.21.2";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
