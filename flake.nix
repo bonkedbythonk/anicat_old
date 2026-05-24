5b82685ac4fe14895f2750cab909340c0e52f82c
Thomas <thomas.rwubben@outlook.com>
2026-05-24 12:12:46 +0200

    chore: version bump to 4.35.10

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.35.9";
+        version = "4.35.11";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
