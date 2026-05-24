c79da8eda1e0389483f3315a856364e2d167253b
Thomas <thomas.rwubben@outlook.com>
2026-05-24 12:06:57 +0200

    chore: sync uv.lock for 4.35.6

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.35.6";
+        version = "4.35.7";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
