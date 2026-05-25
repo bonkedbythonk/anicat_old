85bc3976f1a48bf835e81ff89f95c398cf6f50f7
Thomas <thomas.rwubben@outlook.com>
2026-05-25 10:53:07 +0200

    fix: automatically clean up older version tags and releases during build

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.35.18";
+        version = "4.35.19";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
