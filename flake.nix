a64e8e4d2bbba50d75d8d5a979267fb00f4daf14
Thomas <thomas.rwubben@outlook.com>
2026-05-26 20:53:55 +0200

    style: replace sakura petal with 3x higher quality asset

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.43.0";
+        version = "4.44.0";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
