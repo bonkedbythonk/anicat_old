1496d1f4a9921115c3ecdc5101fd7b85f1879ae8
Thomas <thomas.rwubben@outlook.com>
2026-05-27 09:23:56 +0200

    fix: resolve concurrent release draft race condition in nightly workflow

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib python312Packages;
 
-        version = "4.48.5";
+        version = "4.48.6";
       in
       {
         packages.default = python312Packages.buildPythonApplication {
